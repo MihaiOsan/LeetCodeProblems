825e925bd51dec6c71ad800bd72d4d15c4449dc7
Mihai Osan <mihai.osan1@gmail.com>
2024-12-04 14:28:34 +0200

    prb 67 more efficient using StringBuilder

--- Java/untitled/src/main/java/AddBinary_67.java
+++ Java/untitled/src/main/java/AddBinary_67.java
@@ -5,45 +5,61 @@ public class AddBinary_67 {
         System.out.println(addBinary(a,b));
     }
 
-    public static String addBinary(String a, String b){
-        int lenA = a.length(), lenB = b.length();
-        String sum = "";
-        int rest = 0;
-        char zero ='0';
-        while (lenA > 0 && lenB > 0){
+    public static String addBinary(String a, String b) {
+        StringBuilder sum = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
 
-            int aux = (a.charAt(lenA-1)) -zero + b.charAt(lenB-1) -zero + rest;
-            sum = aux%2 + sum;
-            if (aux >=2)
-                rest = 1;
-            else
-                rest = 0;
-            lenA--;
-            lenB--;
-        }
-        while (lenA > 0){
-            int aux = a.charAt(lenA-1) -zero + rest;
-            sum = aux%2 + sum;
-            if (aux >=2)
-                rest = 1;
-            else
-                rest = 0;
-            lenA--;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digitA = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int digitB = (j >= 0) ? b.charAt(j--) - '0' : 0;
+            int total = digitA + digitB + carry;
+
+            sum.append(total % 2);
+            carry = total / 2;
         }
 
-        while (lenB > 0){
-            int aux = b.charAt(lenB-1) -zero + rest;
-            sum = aux%2 + sum;
-            if (aux >=2)
-                rest = 1;
-            else
-                rest = 0;
-            lenB--;
-        }
-
-        if (rest > 0)
-            sum = rest + sum;
-
-        return sum;
+        return sum.reverse().toString();
     }
+
+//    public static String addBinary(String a, String b){
+//        int lenA = a.length(), lenB = b.length();
+//        String sum = "";
+//        int rest = 0;
+//        char zero ='0';
+//        while (lenA > 0 && lenB > 0){
+//
+//            int aux = (a.charAt(lenA-1)) -zero + b.charAt(lenB-1) -zero + rest;
+//            sum = aux%2 + sum;
+//            if (aux >=2)
+//                rest = 1;
+//            else
+//                rest = 0;
+//            lenA--;
+//            lenB--;
+//        }
+//        while (lenA > 0){
+//            int aux = a.charAt(lenA-1) -zero + rest;
+//            sum = aux%2 + sum;
+//            if (aux >=2)
+//                rest = 1;
+//            else
+//                rest = 0;
+//            lenA--;
+//        }
+//
+//        while (lenB > 0){
+//            int aux = b.charAt(lenB-1) -zero + rest;
+//            sum = aux%2 + sum;
+//            if (aux >=2)
+//                rest = 1;
+//            else
+//                rest = 0;
+//            lenB--;
+//        }
+//
+//        if (rest > 0)
+//            sum = rest + sum;
+//
+//        return sum;
+//    }
 }
